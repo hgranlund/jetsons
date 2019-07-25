15fad1023cafd625b94a07f5b873c86011f03639
Simen Granlund <simen@hgranlund.com>
2019-07-25 21:40:04 +0200

    Updated performance run

--- tests/performanceTest.js
+++ tests/performanceTest.js
@@ -1,16 +1,12 @@
 const { createReadStream, readFileSync, writeFileSync } = require('fs');
-const { toJson, fibonacci, devNullStream } = require('./testUtils');
+const { devNullStream } = require('./testUtils');
 const { JSONStream } = require('../src');
 const { PerformanceTest } = require('../../performaceTester/src');
-// const JsonStreamStringify = require('json-stream-stringify');
 const hugeJson = JSON.parse(readFileSync('tests/data/quotes.json'));
 
-const runJsonStrimifyObject = (obj, useOld = false) => () => {
-  const stream = useOld
-    ? new JsonStreamStringify(obj())
-    : new JSONStream(obj());
-
+const runJsonStrimifyObject = obj => () => {
   return new Promise((resolve, reject) => {
+    const stream = new JSONStream(obj());
     stream.on('end', resolve).on('error', reject);
     stream.pipe(devNullStream());
   });
@@ -30,16 +26,6 @@ const simpleJsonTest = toPerformanceTest(
   'simpleJsonTest',
 );
 
-const simpleJsonTestOld = toPerformanceTest(
-  () => ({
-    test: 1,
-    test2: 'test',
-    test3: [1, 2, 3],
-  }),
-  'simpleJsonOldTest',
-  true,
-);
-
 const jsonWith4MBStringStream = toPerformanceTest(
   () => ({
     lorem: createReadStream('tests/data/loremIpsum-4mb.txt'),
@@ -47,50 +33,28 @@ const jsonWith4MBStringStream = toPerformanceTest(
   'jsonWith4MBStringStream',
 );
 
-const jsonWith4MBStringStreamOld = toPerformanceTest(
-  () => ({
-    lorem: createReadStream('tests/data/loremIpsum-4mb.txt'),
-  }),
-  'jsonWith4MBStringStreamOld',
-  true,
-);
-
 const hugeJsonTest = toPerformanceTest(() => hugeJson, 'hugeJson');
 
-const hugeJsonTestOld = toPerformanceTest(
-  () => hugeJson,
-  'hugeJsonTestOld',
-  true,
-);
-
 const hugeArrayTest = toPerformanceTest(
-  () => Array.from(fibonacci(100000)),
+  () => Array.from(Array.from(new Array(100000))),
   'hugeArray',
-);
-const hugeArrayTestOld = toPerformanceTest(
-  () => Array.from(fibonacci(100000)),
-  'hugeArrayOld',
-  true,
 );
 
 const tests = [
   simpleJsonTest,
-  // simpleJsonTestOld,
   jsonWith4MBStringStream,
-  // jsonWith4MBStringStreamOld,
   hugeJsonTest,
-  // hugeJsonTestOld,
   hugeArrayTest,
-  // hugeArrayTestOld,
 ];
 
 const performanceTests = tests.map(
   ({ name, test }) => new PerformanceTest(test, { name, async: true }),
 );
+
 performanceTests
   .reduce(async (lastProm, test) => {
     await lastProm;
-    return test.run(1);
+    return test.run(50);
   }, Promise.resolve())
   .then(() => {
     const results = performanceTests.map(test => test.result());
@@ -99,30 +63,6 @@ performanceTests
   .catch(error => {
     console.log(error);
   });
-
-const testEquals = () => {
-  const testEqualResult = async obj => {
-    const oldJson = await toJson(new JsonStreamStringify(obj()));
-    const newJson = await toJson(new JsonStreamifyObject(obj()));
-    if (JSON.stringify(oldJson) != JSON.stringify(newJson)) {
-      console.error('new and old are not equal');
-    }
-  };
-  testEqualResult(() => ({
-    test: 1,
-    test2: 'test',
-    test3: [1, 2, 3],
-  }));
-
-  testEqualResult(() => ({
-    lorem: createReadStream('tests/data/loremIpsum-4mb.txt'),
-  }));
-
-  testEqualResult(() => Array.from(fibonacci(100000)));
-  testEqualResult(() => ({
-    lorem: createReadStream('tests/data/loremIpsum-4mb.txt'),
-  }));
-};
 
 const handleResults = results => {
   console.table(results);
@@ -134,10 +74,9 @@ const handleResults = results => {
     readFileSync(`tests/runs/performanceRun.json`),
   );
   console.table(
-    performanceTests.map((test, i) => {
-      const prevResult = prevResults[i];
+    performanceTests.map(test => {
+      const prevResult = prevResults.find(result => result.name === test.name);
       return test.compareWith(prevResult);
     }),
   );
 };
-// testEquals();
